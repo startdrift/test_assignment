a2bbfbba6c11c25cb71af137d64da1178546f70e
startdrift <65728457+startdrift@users.noreply.github.com>
2022-10-04 21:39:46 -0400

    Update apiCall.js

--- services/apiCall.js
+++ services/apiCall.js
@@ -14,10 +14,12 @@ function runRoomba(data) {
             console.log(data);
             console.log(response.data);
 
-            return response.data;
+            return response;
         } catch (error) {
-            console.log(error);
-            throw error;
+            if (error.response) {
+                console.log(error.response.data);
+                return error.response;
+            }
         }
     };
 
