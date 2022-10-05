6424beb1643634de9bcb822742e97279675c73f6
startdrift <65728457+startdrift@users.noreply.github.com>
2022-10-04 21:38:44 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -13,33 +13,38 @@ const {
     countAllPatches,
     countAllPatchesExpectedResponse,
     randomPatches,
-    randomPatchesExpectedResponse
+    randomPatchesExpectedResponse,
+    emptyKeysArray
 } = require('../fixtures/roombaData');
 
 describe('Test Roomba', function () {
     let actualResponse = null;
     it('Verify room dimensions with final hoover position and patches of dirt by sweeping full room', async () => {
         actualResponse = await apiCall.runRoomba(sweepFullRoom);
-        assert.equal(JSON.stringify(actualResponse.coords), sweepFullRoomExpectedResponse);
+        assert.equal(JSON.stringify(actualResponse.data.coords), sweepFullRoomExpectedResponse);
     });
     it('Verify if patch is clean for the remainder of the program run and hoover back to original position by sweeping full room back and forth', async () => {
         actualResponse = await apiCall.runRoomba(sweepFullRoomBackAndForth);
-        assert.equal(JSON.stringify(actualResponse.coords), sweepFullRoomBackAndForthExpectedResponse);
+        assert.equal(JSON.stringify(actualResponse.data.coords), sweepFullRoomBackAndForthExpectedResponse);
     });
     it('Verify if wall has no effect and the robot skids in place by increasing number of steps', async () => {
         actualResponse = await apiCall.runRoomba(sweepFullRoomWithExtraSteps);
-        assert.equal(JSON.stringify(actualResponse.coords), sweepFullRoomWithExtraStepsExpectedResponse);
+        assert.equal(JSON.stringify(actualResponse.data.coords), sweepFullRoomWithExtraStepsExpectedResponse);
     });
     it('Verify all possible patches', async () => {
         actualResponse = await apiCall.runRoomba(countAllPatches);
-        assert.equal(JSON.stringify(actualResponse), countAllPatchesExpectedResponse);
+        assert.equal(JSON.stringify(actualResponse.data), countAllPatchesExpectedResponse);
     });
     it('Verify random patches', async () => {
         actualResponse = await apiCall.runRoomba(randomPatches);
-        assert.equal(JSON.stringify(actualResponse), randomPatchesExpectedResponse);
+        assert.equal(JSON.stringify(actualResponse.data), randomPatchesExpectedResponse);
     });
-    // TODO: Verify response with an empty instructions array
-    // TODO: Verify response with an empty array for all keys
-    // TODO:
-    // TODO:
+    it('Verify random patches', async () => {
+        actualResponse = await apiCall.runRoomba(randomPatches);
+        assert.equal(JSON.stringify(actualResponse.data), randomPatchesExpectedResponse);
+    });
+    it('Verify Bad Request 400 response with an empty array for all keys', async () => {
+        actualResponse = await apiCall.runRoomba(emptyKeysArray);
+        assert.equal(JSON.stringify(actualResponse.status), 500);
+    });
 });
